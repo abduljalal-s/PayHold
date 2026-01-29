71e3c237b67e9a6075bd7ba2b8ec0cec2ee70645
abduljalal-s <nightmadecoder@gmail.com>
2026-01-29 03:09:38 +0100

    chore: disable eslint during build

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
 	reactStrictMode: true,
 	// Optimize for Nigerian bandwidth
 	images: {
